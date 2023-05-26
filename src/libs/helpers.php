511506e5c6d06d19873aeb16160f959c8fa9b914
GitDucMinh <21ducminh@gmail.com>
2023-05-26 21:02:15 +0700

    style form register

---
+++ src/libs/helpers.php
@@ -0,0 +1,11 @@
+<?php
+
+function view(string $filename, array $data = [])
+{
+    foreach($data as $key => $value)
+    {
+        $$key = $value;
+    }
+
+    require_once __DIR__ . '/../inc/' . $filename . '.php';
+}
