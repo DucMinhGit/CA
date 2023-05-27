7187abd20a3b387c9b5725987a14ef44326b3bd2
GitDucMinh <21ducminh@gmail.com>
2023-05-27 10:43:17 +0700

    Redirect

--- src/libs/helpers.php
+++ src/libs/helpers.php
@@ -19,3 +19,25 @@ function is_get_request():bool
 {
     return strtoupper($_SERVER['REQUEST_METHOD'] === 'GET');
 }
+
+function redirect_to(string $url): void
+{
+    header('Location:' . $url);
+    exit;
+}
+
+function redirect_with(string $url, array $items): void
+{
+    foreach ($items as $key => $value) {
+        $_SESSION[$key] = $value;
+    }
+
+    redirect_to($url);
+}
+
+function redirect_with_message(string $url, string $message, string $type=FLASH_SUCCESS)
+{
+    flash('flash_' . uniqid(), $message, $type);
+    redirect_to($url);
+
+}
