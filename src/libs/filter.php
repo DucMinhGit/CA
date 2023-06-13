4ce942e78c4557800226265f373174ca9b73e3dc
GitDucMinh <21ducminh@gmail.com>
2023-06-13 17:12:36 +0700

    filter data input

---
+++ src/libs/filter.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Sanitize and validate data
+ * @param array $data
+ * @param array $fields
+ * @param array $messages
+ * @return array
+ */
+function filter(array $data, array $fields, array $messages = []): array
+{
+    $validation_rule = [];
+    $sanitization_rule = [];
+
+    foreach ($fields as $field => $rules) {
+        if (strpos($rules, '|')) {
+            [$sanitization_rule[$field], $validation_rule[$field]] = explode('|', $rules, 2);
+        }
+    }
+
+    $inputs = sanitize($data, $sanitization_rule);
+    $errors = validate($inputs, $validation_rule);
+
+    return [$inputs, $errors];
+}
